a178ab4dc2ccae4c4fb13384e1b5ad734b857ee7
Neko Davis <neko.davis@mymona.uwi.edu>
2018-02-19 07:40:27 +0000

    second commit

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'flaskmailtest'
 app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
 app.config['MAIL_PORT'] = '465'
-app.config['MAIL_USERNAME'] = '53b71d9ec1501e'
-app.config['MAIL_PASSWORD'] = '077e5c798efbb9'
+app.config['MAIL_USERNAME'] = ''
+app.config['MAIL_PASSWORD'] = ''
 
 mail = Mail(app)
 from app import views
